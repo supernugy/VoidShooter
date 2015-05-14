b92d7d0cf827c98f53983763964d61e12d7b176b
Tomáš Nguyen <nugy@ekonomy.sk>
2015-05-14 08:29:18 +0200

    Power ups
    New power up assets, two new power ups: shield regen and invincibility.

--- powerup.lua
+++ powerup.lua
@@ -3,11 +3,17 @@
 --    (See accompanying file LICENSE_1_0.txt or copy at
 --          http://www.boost.org/LICENSE_1_0.txt)
 
+powerUpShieldImg = nil
+powerUpInvinvImg = nil
+powerUpTypes = {1,2}
+
 function powerUpSpawn(x, y, type)
   local newPowerup = nil
 
   if type == 1 then
-    newPowerup = {x = x, y = y, speed = 300, type = type, img = nil}
+    newPowerup = {x = x, y = y, speed = 300, type = type, img = powerUpShieldImg}
+  elseif type == 2 then
+    newPowerup = {x = x, y = y, speed = 300, type = type, img = powerUpInvinvImg}
   end
 
   return newPowerup
